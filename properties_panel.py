126d2454b36d63c87d6a720295fa48e88fa85b36
Major Péter <majorpeter29@gmail.com>
2018-06-16 18:38:42 +0200

    added vertical spacer to property panel

--- properties_panel.py
+++ properties_panel.py
@@ -1,8 +1,10 @@
 from PyQt5.QtCore import QObject
-from PyQt5.QtWidgets import QGridLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QGridLayout, QLabel, QPushButton, QSpacerItem, QSizePolicy
 
 
 class PropertiesPanel(QObject):
+    COLS_COUNT = 3
+
     def __init__(self, parent):
         super(PropertiesPanel, self).__init__(parent)
         self.grid = QGridLayout(parent)
@@ -11,11 +13,17 @@ class PropertiesPanel(QObject):
         self.clear_layout()
 
         manual_label = QLabel(node.get_node_manual())
-        self.grid.addWidget(manual_label, 0, 0)
+        self.grid.addWidget(manual_label, 0, 0, 1, PropertiesPanel.COLS_COUNT)
 
         props = node.get_properties()
-        for i in range(len(props)):
-            self.grid.addWidget(QLabel(props[i].data['name']), i + 1, 0)
+        property_index = 0
+        for prop in props:
+            name_label = QLabel(prop.data['name'])
+            self.grid.addWidget(name_label, property_index + 1, 0)
+            property_index += 1
+
+        vertical_fill = QSpacerItem(1, 1, QSizePolicy.Fixed, QSizePolicy.Expanding)
+        self.grid.addItem(vertical_fill, property_index + 2, 0, 1, PropertiesPanel.COLS_COUNT)
 
     def clear_layout(self):
         while not self.grid.isEmpty():
