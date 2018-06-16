f8f152b211b698a3c5beef787e206f48c8369b59
Major Péter <majorpeter29@gmail.com>
2018-06-16 18:23:57 +0200

    add node properties to panel

--- properties_panel.py
+++ properties_panel.py
@@ -9,8 +9,13 @@ class PropertiesPanel(QObject):
 
     def display_node(self, node):
         self.clear_layout()
+
         manual_label = QLabel(node.get_node_manual())
-        self.grid.addWidget(manual_label)
+        self.grid.addWidget(manual_label, 0, 0)
+
+        props = node.get_properties()
+        for i in range(len(props)):
+            self.grid.addWidget(QLabel(props[i].data['name']), i + 1, 0)
 
     def clear_layout(self):
         while not self.grid.isEmpty():
