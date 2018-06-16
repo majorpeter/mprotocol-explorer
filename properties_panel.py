b9bc0b8fb45615e29c0444355123b92246927b3f
Major Péter <majorpeter29@gmail.com>
2018-06-16 18:58:09 +0200

    implemented property SET from panel

--- properties_panel.py
+++ properties_panel.py
@@ -5,13 +5,16 @@ from PyQt5.QtWidgets import QGridLayout, QLabel, QPushButton, QSpacerItem, QSize
 class PropertiesPanel(QObject):
     COLS_COUNT = 5
     PROPERTY_PROPNAME = 'propName'
+    PROPERTY_EDITOR = 'editor'
 
     def __init__(self, parent):
         super(PropertiesPanel, self).__init__(parent)
         self.grid = QGridLayout(parent)
+        self.node = None
 
     def display_node(self, node):
         self.clear_layout()
+        self.node = node
 
         manual_label = QLabel(node.get_node_manual())
         self.grid.addWidget(manual_label, 0, 0, 1, PropertiesPanel.COLS_COUNT)
@@ -35,11 +38,14 @@ class PropertiesPanel(QObject):
                 set_button = QPushButton()
                 set_button.setText('Set')
                 set_button.setProperty(PropertiesPanel.PROPERTY_PROPNAME, prop.data['name'])
+                set_button.setProperty(PropertiesPanel.PROPERTY_EDITOR, editor)
+                set_button.clicked.connect(self.on_set_button_pushed)
                 self.grid.addWidget(set_button, property_index + 1, 3)
             if prop.data['type'] == 'METHOD':
                 call_button = QPushButton()
                 call_button.setText('Call')
                 call_button.setProperty(PropertiesPanel.PROPERTY_PROPNAME, prop.data['name'])
+                call_button.setProperty(PropertiesPanel.PROPERTY_EDITOR, editor)
                 self.grid.addWidget(call_button, property_index + 1, 3)
 
             man_button = QPushButton()
@@ -50,6 +56,11 @@ class PropertiesPanel(QObject):
 
         vertical_fill = QSpacerItem(1, 1, QSizePolicy.Fixed, QSizePolicy.Expanding)
         self.grid.addItem(vertical_fill, property_index + 2, 0, 1, PropertiesPanel.COLS_COUNT)
+
+    def on_set_button_pushed(self):
+        property_name = self.sender().property(PropertiesPanel.PROPERTY_PROPNAME)
+        property_value = self.sender().property(PropertiesPanel.PROPERTY_EDITOR).text()
+        self.node[property_name] = property_value
 
     def clear_layout(self):
         while not self.grid.isEmpty():
