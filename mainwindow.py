6171614595746efc7d68b69ee9785f5c60e8620c
Major Péter <majorpeter29@gmail.com>
2018-06-16 16:16:23 +0200

    added connect dialog

--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,7 @@
 import webbrowser
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QInputDialog
 
 from mprotocol_client_python.Client import Client
 from node_model import NodeModel
@@ -21,17 +21,23 @@ class MainWindow(QMainWindow):
 
         self.ui = uic.loadUi('mainwindow.ui', self)
 
-        self.ui.nodeTree.setModel(NodeModel(root_node=self.client.root, parent=self))
-        self.ui.nodeTree.selectionModel().currentChanged.connect(self.node_tree_selection_changed)
-
-        #self.ui.actionConnect_to.triggered.connect(lambda : self.connect_to_device()) TODO
+        self.ui.actionConnect_to.triggered.connect(self.connect_dialog)
         self.ui.actionOpen_protocol_specification.triggered.connect(lambda : webbrowser.open(MainWindow.PROTOCOL_SPEC_URL))
         self.ui.actionAbout.triggered.connect(lambda : QMessageBox.information(self, 'About', MainWindow.ABOUTBOX_MESSAGE, QMessageBox.Ok))
         self.ui.actionExit.triggered.connect(lambda : self.close())
 
+    def connect_dialog(self):
+        entry, ok = QInputDialog.getText(self, 'Connect to device...', 'Enter IP address and port (<ip>:<port>)')
+        if ok:
+            entry = entry.split(':')
+            ip = entry[0]
+            port = int(entry[1])
+            self.connect_to_device(ip, port)
+
     def connect_to_device(self, ip, port):
-        #TODO
-        pass
+        self.client = Client(ip, port, timeout=1)
+        self.ui.nodeTree.setModel(NodeModel(root_node=self.client.root, parent=self))
+        self.ui.nodeTree.selectionModel().currentChanged.connect(self.node_tree_selection_changed)
 
     def node_tree_selection_changed(self, current_index, prev_index):
         print(current_index.internalPointer().get_name())
